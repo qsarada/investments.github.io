664696d755e4aec484051e8478045f3b7e45f759
qsarada <43794447+qsarada@users.noreply.github.com>
2018-11-11 14:29:10 +0300

    merellyne
    merellyne

---
+++ web/js/main.js
@@ -0,0 +1,34 @@
+
+$(function () {
+$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
+
+$(window).scroll(function(){
+   $('.mov').each(function(){
+    var imagePos = $(this).offset().top;
+    var topOfWindow = $(window).scrollTop() ;
+    if (imagePos > topOfWindow){
+      $(this).addClass('bounceInLeft')
+    }
+   });
+});
+$(function(){
+  $('a[href^="#"]').on('click', function(event) {
+    // отменяем стандартное действие
+    event.preventDefault();
+    
+    var sc = $(this).attr("href"),
+        dn = $(sc).offset().top;
+    /*
+    * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+    * dn - определяем положение блока на странице
+    */
+    
+    $('html, body').animate({scrollTop: dn},500);
+    
+    /*
+    * 1000 скорость перехода в миллисекундах
+    */
+  });
+});
+ 
